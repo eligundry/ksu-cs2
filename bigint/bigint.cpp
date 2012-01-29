065d40c667bd8fc371e53c7231d410538545f25d
Eli Gundry <eligundry@gmail.com>
2012-01-29 17:21:47 -0500

    Added my bigint constructers

--- bigint/bigint.cpp
+++ bigint/bigint.cpp
@@ -6,3 +6,65 @@
  * Link        : http://classes.cs.kent.edu/courses/cs33001_lab/svn/Labs/Lab_02/index.html
  *
  */
+
+#include <iostream>
+#include <cstring>
+#include "bigint.h"
+
+// Creates bigint with value 0
+void bigint::zero()
+{
+	for (int i = 0; i < MAX_SIZE; ++i) {
+		digits[i] = 0;
+	}
+}
+
+// Initializes bigint to 0
+bigint::bigint()
+{
+	zero();
+}
+
+// Initializes bigint to whatever number you pass to it
+bigint::bigint(int x)
+{
+	zero();
+
+	for (int i = 0; x != 0; ++i) {
+		digits[i] = x % 10;
+		x /= 10;
+	}
+}
+
+// Initalizes bigint to x, where x is a string
+bigint::bigint(char* x)
+{
+	zero();
+
+	for (int i = 0, y = strlen(x); x[i] != '\0'; ++i, --y) {
+		digits[y] = (int(x[i]) - int('0'));
+	}
+}
+
+bool bigint::operator==(const bigint& x)
+{
+	for (int i = 0; i < MAX_SIZE; i++) {
+		if (x.digits[i] != digits[i]) return false;
+	}
+
+	return true;
+}
+
+bool bigint::operator==(int x)
+{
+	return true;
+}
+
+bool bigint::operator==(const char x[])
+{
+	for (int i = 0, y = strlen(x); x[i] != '\0'; ++i, --y) {
+		if (digits[y] != int(x[i]) - int('0')) return false;
+	}
+
+	return true;
+}
--- bigint/bigint.h
+++ bigint/bigint.h
@@ -1,11 +1,34 @@
-class BigInt()
+#ifndef CS33001_BIGINT_H
+#define CS33001_BIGINT_H
+
+#include <iostream>
+using std::ostream;
+
+const int MAX_SIZE = 100;
+
+class bigint
 {
 public:
 	// Initialize bigint to 0
 	// Example: bigint number();
-	bigint bigint(); 
+	bigint(); 
 	// Initialize bigint to size x
 	// Example: bigint number(30);
-	bigint bigint(int x);
+	bigint(int);
+	// Initialize bigint to size x, where x is a character array
+	// Example: bigint number("30");
+	bigint(char[]);
+
+	// Compares two bigints
+	bool operator==(const bigint&);
+	// Compares two bigints
+	bool operator==(int);
+	// Compares two bigints, where bigint is an array
+	bool operator==(const char[]);
 private:
+	int digits[MAX_SIZE];
+	// Creates bigint with value zero
+	void zero();
 };
+
+#endif
